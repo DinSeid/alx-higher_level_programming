e485a50de5017319f91d8ca64413efe04b7e8aed
dinseid <dinzn@yahoo.com>
2023-04-05 00:24:51 -0700

    modified 100-matrix..

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,62 +1,140 @@
-#!/usr/bin/python3
-# 6-max_integer_test.py
-"""Unittests for max_integer([..])."""
+The ``matrix_mul`` module
+==========================
 
-import unittest
-max_integer = __import__('6-max_integer').max_integer
+Using ``matrix_mul``
+---------------------
+
+First import ``matrix_mul`` from the ``matrix_mul`` module:
+
+	>>> matrix_mul = __import__('100-matrix_mul').matrix_mul
+
+Now use it:
+
+	>>> matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
+	[[7, 10], [15, 22]]
+
+	>>> matrix_mul([[1, 2]], [[3, 4], [5, 6]])
+	[[13, 16]]
+	
+	>>> matrix_mul([[1, 2, 3]], [[3, 4, 5], [5, 6, 8], [9, 10, 11]])
+	[[40, 46, 54]]
+
+	>>> matrix_mul([[2]], [[7]])
+	[[14]]
 
 
-class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+	>>> matrix_mul(9, [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: m_a must be a list
 
-    def test_ordered_list(self):
-        """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+	>>> matrix_mul([[1, 2], [3, 4]], 9)
+	Traceback (most recent call last):
+	...
+	TypeError: m_b must be a list
 
-    def test_unordered_list(self):
-        """Test an unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
+	>>> matrix_mul(9, 9)
+	Traceback (most recent call last):
+	...
+	TypeError: m_a must be a list
 
-    def test_max_at_begginning(self):
-        """Test a list with a beginning max value."""
-        max_at_beginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(max_at_beginning), 4)
 
-    def test_empty_list(self):
-        """Test an empty list."""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
+	>>> matrix_mul([1, 2, 3, 4], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: m_a must be a list of lists
 
-    def test_one_element_list(self):
-        """Test a list with a single element."""
-        one_element = [7]
-        self.assertEqual(max_integer(one_element), 7)
+	>>> matrix_mul([[1, 2], [3, 4]], [1, 2, 3, 4])
+	Traceback (most recent call last):
+	...
+	TypeError: m_b must be a list of lists
 
-    def test_floats(self):
-        """Test a list of floats."""
-        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
-        self.assertEqual(max_integer(floats), 15.2)
+	>>> matrix_mul([1, 2, 3, 4], [1, 2, 3, 4])
+	Traceback (most recent call last):
+	...
+	TypeError: m_a must be a list of lists
 
-    def test_ints_and_floats(self):
-        """Test a list of ints and floats."""
-        ints_and_floats = [1.53, 15.5, -9, 15, 6]
-        self.assertEqual(max_integer(ints_and_floats), 15.5)
 
-    def test_string(self):
-        """Test a string."""
-        string = "Brennan"
-        self.assertEqual(max_integer(string), 'r')
+	>>> matrix_mul([], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a can't be empty
 
-    def test_list_of_strings(self):
-        """Test a list of strings."""
-        strings = ["Brennan", "is", "my", "name"]
-        self.assertEqual(max_integer(strings), "name")
+	>>> matrix_mul([[]], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a can't be empty
 
-    def test_empty_string(self):
-        """Test an empty string."""
-        self.assertEqual(max_integer(""), None)
+	>>> matrix_mul([[1, 2], [3, 4]], [])
+	Traceback (most recent call last):
+	...
+	ValueError: m_b can't be empty
 
-if __name__ == '__main__':
-    unittest.main()
+	>>> matrix_mul([[1, 2], [3, 4]], [[]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_b can't be empty
+
+	>>> matrix_mul([], [])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a can't be empty
+
+	>>> matrix_mul([[]], [[]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a can't be empty
+
+	
+	>>> matrix_mul([[1, 2], ["Betty", 4]], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: m_a should contain only integers or floats
+
+	>>> matrix_mul([[1, 2], [3, 4]], [[1, "Holberton"], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: m_b should contain only integers or floats
+
+	>>> matrix_mul([[1, 2], [None, 4]], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: m_a should contain only integers or floats
+
+
+	>>> matrix_mul([[1, 2], [3]], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: each row of m_a must be of the same size
+
+	>>> matrix_mul([[1, 2], [3, 4]], [[1], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: each row of m_b must be of the same size
+
+	>>> matrix_mul([[1, 2], [3]], [[1], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: each row of m_a must be of the same size
+
+
+	>>> matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4], [5, 6]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a and m_b can't be multiplied
+
+	>>> matrix_mul([[1, 2, 3], [3, 4, 5]], [[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	ValueError: m_a and m_b can't be multiplied
+
+
+	>>> matrix_mul()
+	Traceback (most recent call last):
+	...
+	TypeError: matrix_mul() missing 2 required positional arguments: 'm_a' and 'm_b'
+
+	>>> matrix_mul([[1, 2], [3, 4]])
+	Traceback (most recent call last):
+	...
+	TypeError: matrix_mul() missing 1 required positional argument: 'm_b'
